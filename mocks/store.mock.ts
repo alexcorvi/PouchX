dbf373f63e4aba47d4ac31ded6a143ec6d062214
Alex Corvi <ali.a.saleem@outlook.com>
2019-08-04 17:23:53 +0300

    removed sort func from mock

--- mocks/store.mock.ts
+++ mocks/store.mock.ts
@@ -3,7 +3,4 @@ import { Store } from "../src";
 
 export class Employees extends Store<EmployeeSchema, Employee> {
 	async deleteAccessories() {}
-	sort(a: Employee, b: Employee) {
-		return a.name.localeCompare(b.name);
-	}
 }
